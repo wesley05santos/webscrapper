8d383673944a375166acd3ee9ac0deeb69c689aa
Wesley Santos <wesley05santos@gmail.com>
2024-08-12 20:17:00 -0300

    WIP - Add test files at profiles controller

--- spec/controllers/profiles_spec.rb
+++ spec/controllers/profiles_spec.rb
@@ -17,4 +17,21 @@ RSpec.describe ProfilesController do
       expect(assigns(:profile)).to eq(Profile.first)
     end
   end
+
+  describe 'POST :create' do
+    it 'create one profile' do
+      post :create, params:{profile: {name: 'Teste 999', url: 'teste999'}}
+      expect(response).to redirect_to(profile_path(Profile.last))
+      expect(assigns(:profile)).to eq(Profile.last)
+    end
+  end
+
+  describe 'PUT :update' do
+    it 'update one profile' do
+      put :update, params: {id: Profile.first.id, profile: {name: 'Teste Modificado', url: 'Modificado'}}
+      expect(response).to redirect_to(profile_path(Profile.first))
+      expect(assigns(:profile)).to eq(Profile.first)
+      expect(Profile.where(id: Profile.first.id).pluck(:url, :name).flatten).to include('https://github.com/Modificado', 'Teste Modificado')
+    end
+  end
 end
