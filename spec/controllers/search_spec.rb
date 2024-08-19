e09cd04edfd92f5abb35f95d207887867c2c2f0d
Wesley Santos <wesley05santos@gmail.com>
2024-08-19 18:35:12 -0300

    Tefact  test files to search controller

--- spec/controllers/search_spec.rb
+++ spec/controllers/search_spec.rb
@@ -10,25 +10,25 @@ RSpec.describe SearchController do
   end
 
   describe 'GET :search_profiles' do
-    it 'render all profiles' do
+    it 'all profiles' do
       get :search_profiles
       expect(response).to render_template('search_profiles')
       expect(assigns(:profiles).length).to eq(4)
     end
 
-    it 'search profiles fill in with letters "ano" ' do
+    it 'fill in with letters "ano" ' do
       get :search_profiles, params: {query: 'ano'}
       expect(response).to render_template('search_profiles')
       expect(assigns(:profiles).pluck(:name)).to include('Fulano', 'Ciclano')
     end
 
-    it 'search profiles fill in with letter "e" ' do
+    it 'fill in with letter "e" ' do
       get :search_profiles, params: {query: 'e'}
       expect(response).to render_template('search_profiles')
       expect(assigns(:profiles).pluck(:name)).to include('wesley', 'EDSON')
     end
 
-    it 'search profiles fill in with number "5" ' do
+    it 'fill in with number "5" ' do
       get :search_profiles, params: {query: '5'}
       expect(response).to render_template('search_profiles')
       expect(assigns(:profiles).pluck(:name)).to include('wesley', 'Ciclano')
